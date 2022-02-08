9765a43536d07174e54bb5ded22cb16913bd16e2
The X <haroune.official2@gmail.com>
2022-02-08 19:01:12 +0100

    add: survey answer validation function

--- NeoSurveys-Backend/middleware/InputValidation.js
+++ NeoSurveys-Backend/middleware/InputValidation.js
@@ -12,3 +12,16 @@ module.exports.validateSurvey = (input)=>{
 
     return schema.validate(input);
 }
+
+module.exports.validateAnswer = (input)=>{
+
+    const schema = Joi.object({
+        idSurvey: Joi.number.required(),
+        answers: Joi.array().items(Joi.object({
+            statement: Joi.string().required(),
+            answer: Joi.boolean().required()
+        })).required().min(3)
+    });
+
+    return schema.validate(input);
+}
