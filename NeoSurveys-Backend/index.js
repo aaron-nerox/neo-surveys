c63fde5615f1dd834e7cf0a3fb80b96b28288064
The X <haroune.official2@gmail.com>
2022-02-08 17:10:37 +0100

    finish: get survey routes

--- NeoSurveys-Backend/index.js
+++ NeoSurveys-Backend/index.js
@@ -1,3 +1,4 @@
+const Joi = require('joi');
 const express = require('express');
 const helmet = require('helmet');
 const morgan = require('morgan');
@@ -9,12 +10,12 @@ const port = process.env.port || 8000;
 const homeRoute = require('./routes/homeRoutes');
 const surveyRoutes = require('./routes/surveyRoutes');
 
-app.use('/', homeRoute);
-app.use('/api/surveys', surveyRoutes);
-
-app.use(express.json);
+app.use(express.json());
 app.use(helmet());
 app.use(morgan("dev"));
+
+app.use('/', homeRoute);
+app.use('/api/surveys', surveyRoutes);
 
 app.listen(port, ()=>{
     checkDB();
--- NeoSurveys-Backend/routes/surveyRoutes.js
+++ NeoSurveys-Backend/routes/surveyRoutes.js
@@ -1,20 +1,28 @@
 const express = require('express');
 const router = express.Router();
 const {readDB, appendDB} = require('../controllers/dataBaseController');
+const {validateSurvey} = require('../middleware/InputValidation');
 
 /**
  * get the list of stored surveys
  */
 router.get('/', async (req, res)=>{
-    
+    const surveyList = await readDB();
+    res.send(surveyList);
 });
 
 
 /**
  * get a survey by its id
  */
-router.get('/:id', (req, res)=>{
-    res.send('a survey detail');
+router.get('/:id', async (req, res)=>{
+    const idSurvey = parseInt(req.params.id);
+    const surveyList = await readDB();
+    const survey = surveyList.find(survey => survey.id === idSurvey);
+
+    if(!survey) return res.status(404).send("A survey with such id does not exist");
+
+    res.send(survey);
 });
 
 /**
@@ -29,22 +37,12 @@ router.post('/:id/result', (req, res)=>{
  * create a survey
  * @param: req contains the survey object to store
  */
-router.post('/create', (req,res)=>{
-    let survey = {
-        "name": "This is the second survey",
-        "questions": [
-            {
-                "statement": "are you blind nigga?",
-                "answer": "yes"
-            },
-            {
-                "statement": "are you blind nigga?",
-                "answer": "yes"
-            }
-        ]
-    }
-    appendDB(survey);
-    res.send('survey created');
+router.post('/create', (req, res)=>{
+    const {error, value} = validateSurvey(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
+    
+    // appendDB(req.body);
+    res.send(req.body);
 });
 
 module.exports = router;
