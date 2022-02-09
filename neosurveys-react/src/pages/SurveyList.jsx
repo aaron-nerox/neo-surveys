6668c76e5c6fa8765f9d1da584f779a5a488a49c
The X <haroune.official2@gmail.com>
2022-02-09 15:46:39 +0100

    add: base survey list page

--- neosurveys-react/src/pages/SurveyList.jsx
+++ neosurveys-react/src/pages/SurveyList.jsx
@@ -2,12 +2,25 @@ import SurveyCard from "../components/SurveyCard";
 
 const SurveyList = ()=>{
     return (
-        <div className="w-4/5 mx-auto grid grid-cols-2">
-            <SurveyCard name="this"/>
-            <SurveyCard name="test"/>
-            <SurveyCard name="test"/>
-            <SurveyCard name="test"/>
-            <SurveyCard name="test"/>
+        <div className="min-h-screen grid content-center justify-items-center">
+            <p className="m-4 mt-6 text-2xl font-bold">Browse surveys and take some of them</p>
+            <div className="w-4/5 my-5 mx-auto grid grid-cols-2">
+                <SurveyCard name="this"/>
+                <SurveyCard name="test"/>
+                <SurveyCard name="test"/>
+                <SurveyCard name="test"/>
+                <SurveyCard name="test"/>
+                <SurveyCard name="this"/>
+                <SurveyCard name="test"/>
+                <SurveyCard name="test"/>
+                <SurveyCard name="test"/>
+                <SurveyCard name="test"/>
+                <SurveyCard name="this"/>
+                <SurveyCard name="test"/>
+                <SurveyCard name="test"/>
+                <SurveyCard name="test"/>
+                <SurveyCard name="test"/>
+            </div>
         </div>
     );
 }
