bbbe892fc0961c3ae9a7d06ff5025df2e6f99bf7
The X <haroune.official2@gmail.com>
2022-02-09 02:29:29 +0100

    finish: home page

--- neosurveys-react/src/pages/Home.jsx
+++ neosurveys-react/src/pages/Home.jsx
@@ -1,7 +1,40 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import AppLogo from '../assets/icons/main_logo.png';
 
 const Home = ()=>{
     return (
-        <h1>This is the home page</h1>
+        <div className="w-full h-screen bg-slate-100 grid 
+            content-center justify-items-center">
+
+            <img className="w-1/6 m-5" src={AppLogo} alt="main_logo" />
+
+            <p className="w-4/5 m-2 font-bold text-2xl text-center">
+                Neo Survey is a simple app that let's you freely create a survey 
+                or take one of the many surveys that we have.
+            </p>
+
+            <p className="w-4/5 m-2 font-medium text-lg text-center">
+                Start enjoying your time with this app now
+            </p>
+
+            <div className='inline-flex flex-row items-center'>
+                <Link 
+                    className="bg-baby-blue px-10 py-4 rounded-3xl m-5
+                        hover:bg-azure transition-all font-bold text-xl text-white 
+                        shadow-lg shadow-baby-blue/50 hover:shadow-azure/40" 
+                    to="/surveys">
+                    Browse surveys
+                </Link>
+                <Link 
+                    className="bg-baby-blue px-10 py-4 rounded-3xl m-5
+                        hover:bg-azure transition-all font-bold text-xl text-white 
+                        shadow-lg shadow-baby-blue/50 hover:shadow-azure/40"
+                    to="/newsurvey">
+                    Create a survey
+                </Link>
+            </div>
+        </div>
     );
 }
 
--- neosurveys-react/tailwind.config.js
+++ neosurveys-react/tailwind.config.js
@@ -3,6 +3,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    colors: {
+      'baby-blue': '#00B3FF',
+      'azure': '#00FFB8',
+      'white': '#FEFEFE'
+    },
     extend: {},
   },
   plugins: [],
