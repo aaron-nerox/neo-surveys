917f636a35c42c84347aae1528fbe56e3619e5a6
The X <haroune.official2@gmail.com>
2022-02-10 14:31:50 +0100

    finish: comments on the project

--- neosurveys-react/src/services/crud.services.js
+++ neosurveys-react/src/services/crud.services.js
@@ -1,12 +1,23 @@
 import axios from 'axios';
 const ROOT_URL = "http://d364-105-235-128-64.ngrok.io/api/surveys";
 
+/**
+ * @LIST_SURVEYS : Endpoint for getting the survey list or a survey
+ * @CREATE_SURVEY : Endpoint for survey creation
+ * @RESULT_SURVEY : Endpoint to submit a survey answer and get the result 
+ */
 export const services = {
     LIST_SURVEYS: `${ROOT_URL}`,
     CREATE_SURVEY: `${ROOT_URL}/create`,
     RESULT_SURVEY: `${ROOT_URL}/result`,
 }
 
+/**
+ * Wrapper function over axios get
+ * @param {*} service: Backend service endpoint
+ * @param {*} param: parameter if available
+ * @returns axios get request promise
+ */
 export const Get = (service, param) => {
     let url;
 
@@ -18,6 +29,13 @@ export const Get = (service, param) => {
     return axios.get(url);
 }
 
+/**
+ * Wrapper function over axios post
+ * @param {*} service: Backend service endpoint
+ * @param {*} data: Data to send to the backend
+ * @param {*} param: parameter if available
+ * @returns axios post request promise
+ */
 export const Post = (service, data, param)=>{
     let url;
 
